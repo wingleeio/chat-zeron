8de5b2429a18479801a1936af016c9701994b965
Wing Lee <contact@winglee.io>
2025-06-13 13:12:37 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-full pb-64 px-12">
+    <div className="flex flex-col gap-6 items-center justify-center h-full pb-64 px-4">
       <h2 className="text-2xl">What's on your mind</h2>
       <PromptInputWithActionsNewChat />
     </div>
