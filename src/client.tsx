cd0b816577d18a7c7821a18676a2f8247ac8f422
sina <20732540+SinaKhalili@users.noreply.github.com>
2025-06-19 01:39:35 -0700

    Add posthog

--- src/client.tsx
+++ src/client.tsx
@@ -1,8 +1,22 @@
 import { hydrateRoot } from 'react-dom/client'
 import { StartClient } from '@tanstack/react-start'
+import { PostHogProvider } from 'posthog-js/react'
 
 import { createRouter } from './router'
+import { env } from './env.client'
 
 const router = createRouter()
 
-hydrateRoot(document, <StartClient router={router} />)
+hydrateRoot(
+  document,
+  <PostHogProvider
+    apiKey={env.VITE_PUBLIC_POSTHOG_KEY}
+    options={{
+      api_host: env.VITE_PUBLIC_POSTHOG_HOST,
+      capture_exceptions: true,
+      debug: import.meta.env.MODE === 'development'
+    }}
+  >
+    <StartClient router={router} />
+  </PostHogProvider>
+)
