2c0ff6534c5923e5ba071a000022e499e9db0611
Wing Lee <contact@winglee.io>
2025-06-21 11:29:58 -0700

    Change logo

--- src/components/icons/zeron.tsx
+++ src/components/icons/zeron.tsx
@@ -3,40 +3,75 @@ import type { SVGProps } from "react";
 export function ZeronIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 3544 3544"
-      version="1.1"
+      width="67"
+      height="73"
+      viewBox="0 0 67 73"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      xmlSpace="preserve"
-      style={{
-        fillRule: "evenodd",
-        clipRule: "evenodd",
-        strokeLinejoin: "round",
-        strokeMiterlimit: 2,
-      }}
-      {...props}
     >
-      <g transform="matrix(1.88096,0,0,1.88096,-1794.82,-2059.91)">
-        <g id="Layer1">
-          <text
-            x="995.817"
-            y="2529.94"
-            style={{
-              fontFamily: "'IBMPlexSerif-Bold', 'IBM Plex Serif', serif",
-              fontWeight: 700,
-              fontSize: "1467.03px",
-              fill: "currentColor",
-            }}
-          >
-            Z
-            <tspan x="1946.45" y="2529.94">
-              e
-            </tspan>
-          </text>
-        </g>
-      </g>
+      <line
+        x1="2.5"
+        y1="2.5"
+        x2="63.5"
+        y2="2.49999"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="2.5"
+        y1="2.5"
+        x2="2.5"
+        y2="70.5"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="64.5"
+        y1="2.5"
+        x2="64.5"
+        y2="70.5"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="12.5"
+        y1="4.5"
+        x2="12.5"
+        y2="68.5"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="54.5838"
+        y1="3.42739"
+        x2="15.4274"
+        y2="69.1323"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="54.5"
+        y1="4.5"
+        x2="54.5"
+        y2="68.5"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
+      <line
+        x1="2.5"
+        y1="70.5"
+        x2="63.5"
+        y2="70.5"
+        stroke="currentColor"
+        stroke-width="5"
+        stroke-linecap="round"
+      />
     </svg>
   );
 }
